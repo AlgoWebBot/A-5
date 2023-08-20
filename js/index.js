71a76f6c0dd916b1abf44e0536939ef41bf26079
AlgoWebBot <shimulzahan636@gmail.com>
2023-08-21 01:42:55 +0600

    almost done

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ let price = 0;
 function buyNowParent(target) {
     const count = document.querySelector('#parentDiv').childElementCount;
     // get the name
-    const getName = target.childNodes[3].childNodes[1].innerText;
+    const getName = target.childNodes[5].childNodes[1].innerText;
     // create elemet
     const li = document.createElement('li');
     li.innerHTML = `
@@ -11,30 +11,40 @@ function buyNowParent(target) {
     // append child
     const parentDiv = document.querySelector('#parentDiv').appendChild(li);
     // set price
-    let getPrice = parseFloat(target.childNodes[3].childNodes[3].innerText.split(" ")[0]);
+    let getPrice = parseFloat(target.childNodes[5].childNodes[3].innerText.split(" ")[0]);
     price = (getPrice + price);
     if (price > 0) {
-        document.querySelector('#buy-btn').removeAttribute('disabled');
-    } if (price > 200) {
-        document.querySelector('#apply-btn').removeAttribute('disabled');
+        document.querySelector('#buy-btn').disabled = false;
+    } if (price >= 200) {
+        document.querySelector('#apply-btn').disabled = false;
     }
     document.querySelector('#price').childNodes[1].childNodes[1].innerText = price.toFixed(2) + ' TK';
-    // discount 
-}
+    // total
+    document.querySelector('#price').childNodes[5].childNodes[1].innerText = price.toFixed(2) + ' TK';
 
+}
 // discount part
 document.querySelector('#apply-btn').addEventListener('click', () => {
     const code = document.querySelector('#coupne-code').value;
-    const discountPrice = (price - (0.2 * price)).toFixed(2)
-    const discount = (price - discountPrice).toFixed(2);
-    document.querySelector('#price').childNodes[3].childNodes[1].innerText = discount + ' TK';
-    document.querySelector('#price').childNodes[5].childNodes[1].innerText = discountPrice + ' TK';
+    if (code === 'SELL200') {
+        const discountPrice = (price - (0.2 * price)).toFixed(2)
+        const discount = (price - discountPrice).toFixed(2);
+        document.querySelector('#price').childNodes[3].childNodes[1].innerText = discount + ' TK';
+        document.querySelector('#price').childNodes[5].childNodes[1].innerText = discountPrice + ' TK';
+    }
+    document.querySelector('#coupne-code').value = '';
 })
-
 // reset class
 function home(target) {
-    const removeStyle = document.querySelector('#parentDiv').style.display = 'none';
-    const resetPrice = document.querySelector('#price').childNodes[1].childNodes[1].style.display = 'none';
-    document.querySelector('#price').childNodes[3].childNodes[1].style.display = 'none';
-    document.querySelector('#price').childNodes[5].childNodes[1].style.display = 'none';
+    console.log(price)
+    price = 0;
+    const removeStyle = document.querySelector('#parentDiv').innerHTML = "";
+    console.log(removeStyle);
+    const resetPrice = document.querySelector('#price').childNodes[1].childNodes[1].innerText = "00 TK";
+    document.querySelector('#price').childNodes[3].childNodes[1].innerText = '00 TK';
+    // total 
+    document.querySelector('#price').childNodes[5].childNodes[1].innerText = '00 TK';
+    document.querySelector('#buy-btn').disabled = true;
+    document.querySelector('#apply-btn').disabled = true;
+
 }
